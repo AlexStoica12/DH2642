b30817f411b16663b61fa3827c4634fc47f930c5
sebastiaanvergunst <sebastiaanvergunst@gmail.com>
2021-11-30 21:44:42 +0100

    Request new API token when token has expired

--- src/js/artsySource.js
+++ src/js/artsySource.js
@@ -1,21 +1,25 @@
 const artsySource = {
+  token: "",
   apiCall(params) {
     return fetch("https://api.artsy.net/api/" + params, {
       method: "GET",
       headers: {
-        //  TODO: token needs to be requested every week, need to do this automatically.
-        "X-Xapp-Token":
-          "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsInN1YmplY3RfYXBwbGljYXRpb24iOiI2MTliYWNlZTlkZmFlODAwMGRmYmVjNDYiLCJleHAiOjE2Mzg5MDY0MDIsImlhdCI6MTYzODMwMTYwMiwiYXVkIjoiNjE5YmFjZWU5ZGZhZTgwMDBkZmJlYzQ2IiwiaXNzIjoiR3Jhdml0eSIsImp0aSI6IjYxYTY3ZmEyMjRhOWRiMDAwYjA5ZDcyMyJ9.3RX65kmYX8KXUxMR_bDaiE1EDFQVYJ2qe5rLni1qdkU",
+        "X-Xapp-Token": this.token,
       },
     }).then((response) => {
-      if (response.status !== 200) {
+      if (response.status === 400) {
+        artsyApiToken
+          .apiCall()
+          .then((r) => (this.token = r.token))
+          .catch((e) => console.log(e));
+      } else if (response.status !== 200) {
         throw new Error(response.statusText);
       }
       return response.json();
     });
   },
   searchArtistID(id) {
-    return artsySource.apiCall("/artists/" + id).then((data) => {
+    return artsySource.apiCall("artists/" + id).then((data) => {
       return data;
     });
   },
@@ -25,8 +29,24 @@ const artsySource = {
     });
   },
   searchArtworks(id) {
-    return artsySource.apiCall("/artworks/" + id).then((data) => {
+    return artsySource.apiCall("artworks/" + id).then((data) => {
       return data;
+    });
+  },
+};
+
+const artsyApiToken = {
+  apiCall() {
+    return fetch(
+      "https://api.artsy.net/api/tokens/xapp_token?client_id=13d34ce7f1970b2cdb6c&client_secret=86fa3ab3fc163a22b49f3ec8944898b0",
+      {
+        method: "POST",
+      }
+    ).then((r) => {
+      if (r.status !== 201) {
+        throw new Error(r.statusText);
+      }
+      return r.json();
     });
   },
 };
