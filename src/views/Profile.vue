61e03439a859b2c455012a7ffdd10006925cba4c
Freja Ling <frejal@kth.se>
2021-12-06 12:52:08 +0100

    working prototype

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,7 +4,7 @@
     <v-toolbar color="white" light>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Search</v-toolbar-title>
+      
 
       <v-spacer></v-spacer>
 
@@ -55,14 +55,16 @@
   </v-card>
 </template>
 <script>
+//import ArtsyModel from '../js/artsyModel';
 // @ is an alias to /src
 //import Results from "../components/Results.vue";
 //import artsySource from "@/js/artsySource";
+//import artsyModel from "@/js/artsyModel";
 
 export default {
   name: "Home",
   mounted() {
-    this.searchArtworks();
+    this.getArtworks();
   },
   data() {
     return {
@@ -71,12 +73,12 @@ export default {
         {
           url: "https://64.media.tumblr.com/14709159e19643734eaa79f7ff8ac06d/f317c58810f9ebc3-f8/s500x750/f4ce4c174c19f8dfaf5dbaefab512e1d9fe77c7b.jpg",
           name: "Watercolor",
-          artist: "artist1",
+          artist: "Bello Cavello",
         },
         {
           url: "https://64.media.tumblr.com/82712c483134d2090dfae4908863ba31/d74b2709f78683cb-d5/s1280x1920/e3dfec4b8a0e039fd1fbc5fa55f6d3ce425f40b9.jpg",
           name: "Godzilla anatomic drawing",
-          artist: "artist2",
+          artist: "Heruka Medori",
         },
         {
           url: "https://64.media.tumblr.com/72d06e38a80b674619ed921f185cfbc4/6777aebfef58aa8e-3d/s500x750/5ea3ca6fe1dd8026a6fdd7af1935bb0e4dede37b.png",
@@ -97,9 +99,9 @@ export default {
     };
   },
   methods: {
-    async searchArtworks() {
-      //let artworks = await artsySource.searchAllArtworks();
-      this.artworks = this.images//artworks._embedded.artworks;
+    async getArtworks() {
+      //let favartworks = await artsyModel.getFavoritedArtworks();
+      this.artworks = this.images
     },
     // Helper function for navigation
     navigateTo: function (route) {
