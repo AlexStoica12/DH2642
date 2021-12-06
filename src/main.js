0baca0af81d4b4a6f6263ba516d6434d20736bd2
eleander <edward.leander@hotmail.com>
2021-12-06 10:23:10 +0100

    fixed bug
    Co-authored-by: AlexStoica12 <armst@kth.se>
    Co-authored-by: sebastiaanvergunst <sebastiaanvergunst@gmail.com>
    Co-authored-by: frejaling <freja.ling@gmail.com>

--- src/main.js
+++ src/main.js
@@ -3,11 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import Viewer from "v-viewer";
-//import infiniteScroll from "vue-infinite-scroll";
 
-//Vue.use(infiniteScroll);
-Vue.use(Viewer);
 Vue.config.productionTip = false;
 
 new Vue({
