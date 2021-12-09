809a18a4ab9868a5f93e8401c2b482733fc7466e
sebastiaanvergunst <sebastiaanvergunst@gmail.com>
2021-12-09 11:22:36 +0100

    Make Profile dynamic

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,40 +1,48 @@
 <template>
   <v-container class="background">
-    <v-container :key="image.url" v-for="(image, i) in images">
-      <!-- <v-container v-if="i % 2 == 0" align = "right" class="images"> -->
-      <v-col v-if="i % 2 == 0" class="images" align="right" contain>
-        <!-- <v-row class="sub-image">      {{image.name}}  {{image.artist}}        -->
-
-        <v-img :src="image.url" width="750px" />
+    <v-container v-if="favoritedArtworks.length === 0">
+      <v-btn color="orange" text @click.stop="navigateHome()">
+        There's nothing in your Gallery yet, click here to explore more
+        paintings..
+      </v-btn>
+    </v-container>
+    <v-container
+      v-else
+      v-for="(artwork, i) in favoritedArtworks"
+      :key="artwork.title"
+    >
+      <v-col v-if="i % 2 === 0" class="images" align="right" contain>
+        <v-btn
+          color="orange"
+          text
+          @click.stop="removeArtworkFromGallery(artwork)"
+        >
+          Remove
+        </v-btn>
+        <v-img :src="artwork._links.thumbnail.href" width="750px" />
         <v-spacer></v-spacer>
-
         <span
           class="text-md-subtitle-2 black--text pl-4 pt-4 d-inline-block"
-          v-text="image.name + ', ' + image.artist"
+          v-text="artwork.title + ', ' + 'Artist Name'"
         ></span>
         <v-divider></v-divider>
-
-        <!-- </v-img> -->
-
-        <!-- </v-row> -->
       </v-col>
-
-      <!-- </v-container> -->
-      <!-- <v-container v-else class="images"> -->
       <v-col v-else class="images">
-        <!-- <img :src="image.url" alt="artwork" align = "left" class="images" @click="directImage"/> -->
-        <v-img :src="image.url" width="500px" contain> </v-img>
-
+        <v-btn
+          color="orange"
+          text
+          @click.stop="removeArtworkFromGallery(artwork)"
+        >
+          Remove
+        </v-btn>
+        <v-img :src="artwork._links.thumbnail.href" width="500px" contain>
+        </v-img>
         <span
           class="text-md-subtitle-2 black--text pl-0 pt-4 d-inline-block"
-          v-text="image.name + ', ' + image.artist"
+          v-text="artwork.title + ', ' + 'Artist Name'"
         ></span>
         <v-divider></v-divider>
-
-        <!-- <v-row class="sub-image">  {{image.name}} {{image.artist}} 
-        </v-row> -->
       </v-col>
-      <!-- </v-container> -->
     </v-container>
   </v-container>
 </template>
@@ -47,31 +55,12 @@ export default {
     },
   },
   methods: {
-    directImage() {
-      //redirect to detailspage
+    removeArtworkFromGallery: function (artwork) {
+      this.$store.dispatch("removeFromFavorited", artwork);
     },
-  },
-
-  data() {
-    return {
-      images: [
-        {
-          url: "https://64.media.tumblr.com/14709159e19643734eaa79f7ff8ac06d/f317c58810f9ebc3-f8/s500x750/f4ce4c174c19f8dfaf5dbaefab512e1d9fe77c7b.jpg",
-          name: "Watercolor",
-          artist: "Bellicio Gambari",
-        },
-        {
-          url: "https://64.media.tumblr.com/27b6c18f4a8e04839708bc73ee5acc7f/67b7cb7a80422330-e1/s500x750/6662562dda7bc96774e1d11d2c19d26b16ef56fe.jpg",
-          name: "Godzilla anatomic drawing",
-          artist: "Harakuro Mitni",
-        },
-        {
-          url: "https://64.media.tumblr.com/72d06e38a80b674619ed921f185cfbc4/6777aebfef58aa8e-3d/s500x750/5ea3ca6fe1dd8026a6fdd7af1935bb0e4dede37b.png",
-          name: "Revenge Cagn",
-          artist: "Lady Skollie",
-        },
-      ],
-    };
+    navigateHome: function () {
+      this.$router.push("/home");
+    },
   },
 };
 </script>
@@ -79,19 +68,5 @@ export default {
 <style scoped>
 .background {
   background: #f5f2f2;
-}
-
-/* .images {
-    line-height: 300px;
-} */
-.br {
-  display: inline-block;
-}
-
-.sub-images {
-  line-height: auto;
-}
-
-.v-img {
 }
 </style>
