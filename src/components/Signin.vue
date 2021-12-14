0fbe6aa0a039dd39d8962b16665a63c7abec7c15
eleander <edward.leander@hotmail.com>
2021-12-14 15:25:15 +0100

    removed unnecessary call to loadUserData

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -93,7 +93,6 @@ export default {
             email: this.email,
             password: this.password,
           })
-          .then(() => this.$store.dispatch("loadUserData"))
           .then(this.closeDialog(false));
       }
     },
